35d76806e03f78d52f77071d4e9b94a5b05f575e
Rumeet Goradia <rumeet.goradia@gmail.com>
2021-05-15 18:25:03 -0400

    Add dialog for calendar events

--- components/Home/Introduction/Calendar/CalendarEvent/CalendarEvent.styles.tsx
+++ components/Home/Introduction/Calendar/CalendarEvent/CalendarEvent.styles.tsx
@@ -1,35 +1,6 @@
-import { fade, makeStyles } from "@material-ui/core/styles"
+import { makeStyles } from "@material-ui/core/styles"
 
 const useCalendarEventStyles = makeStyles((theme) => ({
-	tooltip: {
-		background: fade(theme.palette.background.paper, 0.95),
-		padding: theme.spacing(2),
-		color: theme.palette.text.primary,
-		marginTop: theme.spacing(0.5),
-		position: "relative",
-		boxShadow: theme.shadows[1],
-		cursor: "default",
-	},
-	container: {
-		wordWrap: "break-word",
-	},
-	title: {
-		fontSize: theme.typography.fontSize * 1.25,
-		fontWeight: 500,
-		maxWidth: "100%",
-		position: "relative",
-		zIndex: 16000,
-		borderLeft: `5px solid ${theme.palette.secondary.main}`,
-		paddingLeft: theme.spacing(2),
-		marginBottom: theme.spacing(2),
-	},
-	infoIcon: {},
-	infoContainer: {},
-	info: {
-		maxWidth: 245,
-
-		// display: "inline",
-	},
 	buttonBase: {
 		width: "100%",
 		justifyContent: "flex-start",
@@ -41,6 +12,30 @@ const useCalendarEventStyles = makeStyles((theme) => ({
 	preview: {
 		lineHeight: 1,
 		fontSize: theme.typography.fontSize,
+	},
+	title: {
+		fontSize: theme.typography.fontSize * 1.5,
+		fontWeight: 500,
+		borderLeft: `5px solid ${theme.palette.secondary.main}`,
+		paddingLeft: theme.spacing(2),
+		// marginBottom: theme.spacing(2),
+	},
+	closeButton: {
+		position: "absolute",
+		right: theme.spacing(1),
+		top: theme.spacing(0.75),
+		color: theme.palette.grey[500],
+	},
+	content: {
+		[theme.breakpoints.up("sm")]: {
+			paddingLeft: theme.spacing(6),
+			paddingRight: theme.spacing(6),
+		},
+		paddingBottom: theme.spacing(2),
+		maxWidth: "100%",
+	},
+	infoIcon: {
+		paddingTop: 2,
 	},
 }))
 
--- components/Home/Introduction/Calendar/CalendarEvent/CalendarEvent.tsx
+++ components/Home/Introduction/Calendar/CalendarEvent/CalendarEvent.tsx
@@ -1,7 +1,18 @@
-import { Box, ButtonBase, Grid, Tooltip, Typography } from "@material-ui/core"
-import ClickAwayListener from "@material-ui/core/ClickAwayListener"
+import { Hyperlink } from "@/components/Typography"
+import {
+	Box,
+	ButtonBase,
+	Dialog,
+	DialogContent,
+	DialogTitle,
+	Grid,
+	IconButton,
+	Typography,
+} from "@material-ui/core"
 import type { calendar_v3 } from "googleapis"
 import { useState } from "react"
+import { BsFillClockFill } from "react-icons/bs"
+import { MdClose, MdInfo } from "react-icons/md"
 import { RiMapPinFill } from "react-icons/ri"
 import useCalendarEventStyles from "./CalendarEvent.styles"
 
@@ -9,75 +20,128 @@ interface CalendarEventProps {
 	event: calendar_v3.Schema$Event
 }
 
+const URL_REGEX = new RegExp(
+	/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/i
+)
+
 const CalendarEvent: React.FC<CalendarEventProps> = ({ event }) => {
 	const [open, setOpen] = useState<boolean>(false)
 
-	const classes = useCalendarEventStyles()
-
-	const handleTooltipClose = () => {
-		setOpen(false)
-	}
-
-	const handleTooltipOpen = () => {
+	const handleClickOpen = () => {
 		setOpen(true)
 	}
 
+	const handleClose = () => {
+		setOpen(false)
+	}
+
+	const classes = useCalendarEventStyles()
+
 	return (
-		<ClickAwayListener onClickAway={handleTooltipClose}>
-			<div>
-				<Tooltip
-					PopperProps={{
-						disablePortal: true,
-					}}
-					onClose={handleTooltipClose}
-					open={open}
-					disableFocusListener
-					disableHoverListener
-					disableTouchListener
-					title={
-						<Box width="100%" className={classes.container}>
-							<Typography variant="h5" className={classes.title}>
-								Mollit excepteur anim consectetur ut magna ad laboris sunt
-								consectetur.
-							</Typography>
-							<Grid container spacing={2}>
-								{event.location && (
-									<Grid item xs={12} container spacing={1} wrap="wrap">
-										<Grid item xs={1} zeroMinWidth className={classes.infoIcon}>
-											<RiMapPinFill />
-										</Grid>
-										<Grid item xs={11}>
-											<Typography className={classes.info}>
-												Mollit excepteur anim consectetur ut magna ad laboris
-												sunt consectetur.
-											</Typography>
-										</Grid>
-									</Grid>
-								)}
-							</Grid>
-						</Box>
-					}
-					interactive
-					classes={{ tooltip: classes.tooltip }}
+		<>
+			<ButtonBase onClick={handleClickOpen} className={classes.buttonBase}>
+				<Box
+					bgcolor="primary.main"
+					width="100%"
+					textAlign="left"
+					className={classes.previewContainer}
 				>
-					<ButtonBase
-						onClick={handleTooltipOpen}
-						className={classes.buttonBase}
+					<Typography variant="body1" className={classes.preview}>
+						{event.summary}
+					</Typography>
+				</Box>
+			</ButtonBase>
+			<Dialog
+				onClose={handleClose}
+				open={open}
+				maxWidth="sm"
+				fullWidth
+				aria-labelledby={`${event.summary}-${event.start?.dateTime}-title`}
+			>
+				<DialogTitle
+					disableTypography
+					id={`${event.summary}-${event.start?.dateTime}-title`}
+				>
+					<Typography variant="h5" className={classes.title}>
+						{event.summary}
+					</Typography>
+					<IconButton
+						aria-label="close"
+						className={classes.closeButton}
+						onClick={handleClose}
 					>
-						<Box
-							bgcolor="primary.main"
-							width="100%"
-							textAlign="left"
-							className={classes.previewContainer}
-						>
-							<Typography variant="body1" className={classes.preview}>
-								{event.summary}
-							</Typography>
-						</Box>
-					</ButtonBase>
-				</Tooltip>
-			</div>
-		</ClickAwayListener>
+						<MdClose />
+					</IconButton>
+				</DialogTitle>
+				<DialogContent className={classes.content}>
+					<Grid container spacing={1}>
+						{event.start?.dateTime && (
+							<Grid item xs={12}>
+								<Grid container spacing={2}>
+									<Grid item xs={1}>
+										<Typography className={classes.infoIcon}>
+											<BsFillClockFill />
+										</Typography>
+									</Grid>
+									<Grid item xs={11}>
+										<Typography variant="body1">
+											{new Intl.DateTimeFormat("en-US", {
+												hour12: true,
+												hour: "numeric",
+												minute: "numeric",
+												month: "numeric",
+												day: "numeric",
+												year: "numeric",
+											}).format(new Date(event.start.dateTime))}
+										</Typography>
+									</Grid>
+								</Grid>
+							</Grid>
+						)}
+						{event.location && (
+							<Grid item xs={12}>
+								<Grid container spacing={2}>
+									<Grid item xs={1}>
+										<Typography className={classes.infoIcon}>
+											<RiMapPinFill />
+										</Typography>
+									</Grid>
+									<Grid item xs={11}>
+										<Typography variant="body1">
+											{URL_REGEX.test(event.location) ? (
+												<Hyperlink
+													href={event.location}
+													target="_blank"
+													rel="noopener"
+												>
+													Virtual
+												</Hyperlink>
+											) : (
+												<>{event.location}</>
+											)}
+										</Typography>
+									</Grid>
+								</Grid>
+							</Grid>
+						)}
+						{event.description && (
+							<Grid item xs={12}>
+								<Grid container spacing={2}>
+									<Grid item xs={1}>
+										<Typography className={classes.infoIcon}>
+											<MdInfo />
+										</Typography>
+									</Grid>
+									<Grid item xs={11}>
+										<Typography variant="body1">{event.description}</Typography>
+									</Grid>
+								</Grid>
+							</Grid>
+						)}
+					</Grid>
+				</DialogContent>
+			</Dialog>
+		</>
 	)
 }
 
