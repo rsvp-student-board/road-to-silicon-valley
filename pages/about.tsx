2fb0c03990bca10ebff7e6035052c59488de17c9
Rumeet Goradia <rumeet.goradia@gmail.com>
2021-05-20 16:52:17 -0400

    Try removing mission

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,4 +1,4 @@
-import { Mission, OfferList } from "@/components/About"
+import { OfferList } from "@/components/About"
 import { HeroLayout } from "@/components/Layout"
 import { Content } from "@/components/Typography"
 import { WHAT_RSVP_OFFERS } from "@/content"
@@ -35,7 +35,7 @@ const AboutPage: React.FC<BlurredImgProps> = (imgs) => {
 					"The Road to Silicon V/Alley Program (RSVP) paves the way for students to become leaders in innovation (intrapreneurship) and entrepreneurship in a digital age. This organization aims to capitalize on the potential of Rutgers students and maximize innovation and technological skill on campus.",
 			}}
 		>
-			<Mission />
+			{/* <Mission /> */}
 			<Grid item xs={12}>
 				<Content header="Overview">
 					<Typography variant="body1">
